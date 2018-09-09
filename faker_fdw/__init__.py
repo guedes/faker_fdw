8146da16425608bfad8b99be400851f2bdfd513f
Dickson S. Guedes <guedes@guedesoft.net>
2018-09-09 19:23:43 -0300

    refactoring some default

--- faker_fdw/__init__.py
+++ faker_fdw/__init__.py
@@ -3,26 +3,27 @@
 #
 # This module is free software; you can redistribute it and/or modify it under
 # the [PostgreSQL License](http://www.opensource.org/licenses/postgresql).
-# 
+#
 # Permission to use, copy, modify, and distribute this software and its
 # documentation for any purpose, without fee, and without a written agreement is
 # hereby granted, provided that the above copyright notice and this paragraph
 # and the following two paragraphs appear in all copies.
-# 
+#
 # In no event shall Dickson S. Guedes be liable to any party for direct,
 # indirect, special, incidental, or consequential damages, including lost
 # profits, arising out of the use of this software and its documentation, even
 # if Dickson S. Guedes has been advised of the possibility of such damage.
-# 
+#
 # Dickson S. Guedes specifically disclaims any warranties, including, but not
 # limited to, the implied warranties of merchantability and fitness for a
 # particular purpose. The software provided hereunder is on an "as is" basis,
 # and Dickson S. Guedes has no obligations to provide maintenance, support,
 # updates, enhancements, or modifications.
 #
-from multicorn import ForeignDataWrapper
-from multicorn.utils import log_to_postgres, ERROR, WARNING, DEBUG
+from multicorn import ForeignDataWrapper, TableDefinition, ColumnDefinition
+from multicorn.utils import log_to_postgres, DEBUG
 from faker import Faker
+
 
 class FakerForeignDataWrapper(ForeignDataWrapper):
 
@@ -30,19 +31,22 @@ class FakerForeignDataWrapper(ForeignDataWrapper):
         super(FakerForeignDataWrapper, self).__init__(options, columns)
         log_to_postgres("call __init__()", DEBUG)
 
-        self.limit = 100
-        self.seed = None
-        self.locale = 'en_US'
         self.columns = {}
 
-        if 'max_results' in options:
+        try:
             self.limit = int(options['max_results'])
+        except Exception:
+            self.limit = 100
 
-        if 'locale' in options:
+        try:
             self.locale = options['locale']
+        except Exception:
+            self.locale = 'en_US'
 
-        if 'seed' in options:
+        try:
             self.seed = int(options['seed'])
+        except Exception:
+            self.seed = None
 
         faker = Faker(self.locale)
         faker.seed(self.seed)
