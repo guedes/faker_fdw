56e323732460a1b3537dca553787f523a950083d
Dickson S. Guedes <guedes@guedesoft.net>
2019-09-27 22:00:40 -0300

    Bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name='faker_fdw',
-    version='0.2.2',
+    version='0.2.3',
     author='Dickson S. Guedes',
     license='PostgreSQL',
     packages=['faker_fdw']
